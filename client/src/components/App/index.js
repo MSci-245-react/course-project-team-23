5b813df447e25dc8c6742cacbb1afc11bcb038d1
Jeevan Parmar <93451143+jeevanp03@users.noreply.github.com>
2024-02-14 16:09:27 +0000

    added router

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Review from "../Review";
 
-function App() {
-  return <Review />;
-}
-
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Review />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 export default App;
